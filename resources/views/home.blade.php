4bb639649cae365bcd4888f2600d7229b87cd3e1
Patlobar <titalobar7@gmail.com>
2024-04-04 10:56:38 -0300

    criaçãoo do layout base menu e alteração da home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,63 @@
+<x-layuot_base>
+<x-menu></x-menu>
+
+<div class="carousel w-full">
+  <div id="slide1" class="carousel-item relative w-full">
+    <img src="https://http2.mlstatic.com/D_NQ_797422-MLA75400551648_042024-OO.webp" class="w-full" />
+    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide1" class="btn btn-circle">❮</a> 
+      <a href="#slide2" class="btn btn-circle">❯</a>
+    </div>
+  </div> 
+  <div id="slide2" class="carousel-item relative w-full">
+    <img src="https://http2.mlstatic.com/D_NQ_667836-MLA74349586053_022024-OO.webp" class="w-full" />
+    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide2" class="btn btn-circle">❮</a> 
+      <a href="#slide3" class="btn btn-circle">❯</a>
+    </div>
+  </div> 
+  <div id="slide3" class="carousel-item relative w-full">
+    <img src="https://http2.mlstatic.com/D_NQ_806000-MLA75393863908_042024-OO.webp" class="w-full" />
+    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide3" class="btn btn-circle">❮</a> 
+      <a href="#slide4" class="btn btn-circle">❯</a>
+    </div>
+  </div> 
+  <div id="slide4" class="carousel-item relative w-full">
+    <img src="https://http2.mlstatic.com/D_NQ_945326-MLA75548553029_042024-OO.webp" class="w-full" />
+    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide4" class="btn btn-circle">❮</a> 
+      <a href="#slide5" class="btn btn-circle">❯</a>
+    </div>
+</div>
+    <div id="slide5" class="carousel-item relative w-full">
+    <img src="https://http2.mlstatic.com/D_NQ_755668-MLA75432560149_032024-OO.webp" class="w-full" />
+    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide5" class="btn btn-circle">❮</a> 
+      <a href="#slide6" class="btn btn-circle">❯</a>
+    </div>
+    </div>
+     <div id="slide6" class="carousel-item relative w-full">
+    <img src="https://http2.mlstatic.com/D_NQ_922106-MLA75377276862_042024-OO.webp" class="w-full" />
+    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide6" class="btn btn-circle">❮</a> 
+      <a href="#slide1" class="btn btn-circle">❯</a>
+    </div>
+  </div>
+</div>
+<div class="bg-blue-200"  >
+
+
+
+
+
+</div>
+
+
+
+
+
+
+
+
+</x-layuot_base>
